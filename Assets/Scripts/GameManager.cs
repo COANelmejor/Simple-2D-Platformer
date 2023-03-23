24b9d33609785a57b6dd5452af70e88c8a3fb86a
Christian Obed Arana navas <COANelmejor@gmail.com>
2023-03-23 00:23:54 -0600

    Updated when StartGame can be executed - Now, it only can be modified if the GameState is not "inGame"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,7 +30,7 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Return)) {  
+        if (Input.GetKeyDown(KeyCode.Return) && currentGameState != GameState.inGame) {  
             StartGame();
         }
     }
